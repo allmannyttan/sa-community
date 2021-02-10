a7262697e275ca3458266ce6c189b44009737ec5
momentiris <momentiris@gmail.com>
2021-02-10 17:18:58 +0100

    improvement: swedish description for table of contents

--- studio/schemas/tableOfContents.js
+++ studio/schemas/tableOfContents.js
@@ -3,5 +3,5 @@ export default {
   type: 'boolean',
   name: 'tableOfContents',
   description:
-    'If "On", the page will have a sidebar with a table of contents section that the user can click and get redirected to a certain section on the page. This is done automatically by generating links from all of the headings in the article.',
+    'Om "On" så kommer sidan ha en sidomeny med en innehållsförteckning baserad på de heading-taggar som finns i "Body". Besökaren kan då navigera runt i texten genom att klicka på dessa.',
 }
