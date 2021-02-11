9c48f042d2cfe3289abcaee0d143ce69084c81a9
momentiris <momentiris@gmail.com>
2021-02-11 10:32:01 +0100

    fix(web): only use one static query

--- web/src/pages/projekt.js
+++ web/src/pages/projekt.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 import BlockContent from '../components/blockContent'
 
 const query = graphql`
-  query projects {
+  query projectsPage {
     sanityProjectPage {
       _rawBody
       title
@@ -25,10 +25,8 @@ const query = graphql`
 `
 
 const Component = () => {
-  const posts = useStaticQuery(query).allSanityProject.edges.map(
-    ({ node }) => node
-  )
-  const data = useStaticQuery(query).sanityProjectPage
+  const { sanityProjectPage: data, allSanityProject } = useStaticQuery(query)
+  const projects = allSanityProject.edges.map(({ node }) => node) || []
 
   return (
     <Layout>
@@ -36,7 +34,7 @@ const Component = () => {
         <h2 className="text-xl">{data.title}</h2>
         <BlockContent className="text-center" blocks={data._rawBody} />
       </div>
-      {posts.map((project) => (
+      {projects.map((project) => (
         <Link key={project.title} to={`${project.slug.current}`}>
           <p>{project.title}</p>
         </Link>
