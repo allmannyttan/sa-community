a2baa95bff4aa0287e8f092d6b27772d7ac20ab3
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-02-12 09:12:59 +0100

    chore: article if statement

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -22,7 +22,7 @@ const SEO = ({ title, description, article }) => {
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
       {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {article && <meta property="og:type" content="article" />}
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && (
         <meta property="og:description" content={seo.description} />
