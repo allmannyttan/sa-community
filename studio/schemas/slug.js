1c73d855809d69872b907972086a06001f0a604b
momentiris <momentiris@gmail.com>
2021-02-17 10:55:50 +0100

    fix: remove slug validation

--- studio/schemas/slug.js
+++ studio/schemas/slug.js
@@ -6,7 +6,7 @@ export default {
     'Used to create a page for the content. Should be same as title but with dashes instead of space and lowercase. Press [Generate] after you have filled out the title to do this automatically.',
   validation: (Rules) => [
     Rules.required().error('Provide a slug'),
-    Rules.max(96).warning('Shorter slugs are more convenient'),
+    // Rules.max(96).warning('Shorter slugs are more convenient'),
   ],
   options: {
     source: 'title',
