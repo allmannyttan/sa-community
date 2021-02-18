e398811127e54739ef349cd63d395a94e68bff5a
momentiris <momentiris@gmail.com>
2021-02-18 12:19:18 +0100

    chore: edit sitesettings schema for footer

--- studio/schemas/siteSettings.js
+++ studio/schemas/siteSettings.js
@@ -2,22 +2,37 @@ export default {
   name: 'siteSettings',
   type: 'document',
   title: 'Inställningar',
+  fieldsets: [
+    {
+      name: 'social',
+      title: 'Sociala medier och länkar',
+      description: 'Det som fylls i här hamnar i footern.',
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+      description: 'Sökmotorsoptimering',
+    },
+  ],
   fields: [
     {
       name: 'title',
       type: 'string',
       title: 'Namn på sidan',
+      fieldset: 'seo',
     },
     {
       name: 'URL',
       type: 'string',
       title: 'Länk till sidan',
+      fieldset: 'seo',
     },
     {
       name: 'description',
       type: 'text',
       title: 'Beskrivning',
       description: 'Beskriv sidan för sökmotorer och sociala medier.',
+      fieldset: 'seo',
     },
     {
       name: 'keywords',
@@ -28,6 +43,31 @@ export default {
       options: {
         layout: 'tags',
       },
+      fieldset: 'seo',
+    },
+    {
+      name: 'email',
+      type: 'string',
+      title: 'Email',
+      fieldset: 'social',
+    },
+    {
+      name: 'github',
+      type: 'string',
+      title: 'Github',
+      fieldset: 'social',
+    },
+    {
+      name: 'facebook',
+      type: 'string',
+      title: 'Facebook',
+      fieldset: 'social',
+    },
+    {
+      name: 'twitter',
+      type: 'string',
+      title: 'Twitter',
+      fieldset: 'social',
     },
   ],
 }
