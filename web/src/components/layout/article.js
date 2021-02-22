a5768ab4f8c133d332c0cfe3548ed7bdb8d954dc
izabellalarsson <izzwii.izabella@gmail.com>
2021-02-22 07:49:55 +0100

    improvement: set max width

--- web/src/components/layout/article.js
+++ web/src/components/layout/article.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Article = ({ children }) => {
   return (
-    <article className="max-w-screen-md flex flex-col px-6 pt-6 md:pl-12 md:pt-12">
+    <article className="max-w-screen-md flex flex-col px-6 pt-6 md:pl-12 md:pt-12 xl:m-auto">
       {children}
     </article>
   )
--- web/src/components/layout/aside.js
+++ web/src/components/layout/aside.js
@@ -3,8 +3,8 @@ import BreadCrumbs from '../breadcrumbs'
 
 const Aside = ({ children }) => {
   return (
-    <aside className="w-1/5 hidden md:block">
-      <section className="fixed w-1/5 h-screen border-r border-lightGrey pt-10 md:pl-4 ">
+    <aside className="w-1/5 max-w-xs hidden md:block">
+      <section className="fixed w-1/5 max-w-xs h-screen border-r border-lightGrey pt-10 md:pl-4 ">
         <BreadCrumbs />
         {children}
       </section>
