569e65c4cea3ea1176115ba8877bcb74a8433936
izabellalarsson <izzwii.izabella@gmail.com>
2021-02-24 08:39:05 +0100

    chore: add break point for large screens

--- web/src/components/layout/article.js
+++ web/src/components/layout/article.js
@@ -3,7 +3,7 @@ import BreadCrumbs from '../breadcrumbs'
 
 const Article = ({ children }) => {
   return (
-    <article className="max-w-screen-md flex flex-col px-6 pt-6 md:pl-12 md:pt-12 xl:m-auto">
+    <article className="max-w-screen-md flex flex-col px-6 pt-6 md:pl-12 md:pt-12 3xl:m-auto">
       <div className="mb-12">
         <BreadCrumbs />
       </div>
