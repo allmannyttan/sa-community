1de72c5e40094a3f6a4dbe9d1335fb4438ec344e
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-02-24 15:31:00 +0100

    styling: margin bredCrumbs

--- web/src/components/layout/article.js
+++ web/src/components/layout/article.js
@@ -4,7 +4,7 @@ import BreadCrumbs from '../breadcrumbs'
 const Article = ({ children }) => {
   return (
     <article className="max-w-screen-md flex flex-col px-6 pt-6 md:pl-12 md:pt-12 3xl:m-auto">
-      <div className="mb-12">
+      <div className="mb-1 md:mb-4">
         <BreadCrumbs />
       </div>
 
