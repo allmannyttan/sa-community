d115f72bddd57df3a99791031851c0e90e2de4f8
izabellalarsson <izzwii.izabella@gmail.com>
2021-02-24 17:13:58 +0100

    chore: clean up

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,6 +3,7 @@ import { graphql, Link, useStaticQuery } from 'gatsby'
 import SEO from '../components/seo'
 import bg from '../images/bg.svg'
 import Img from 'gatsby-image'
+import * as Typography from '../components/typography'
 
 const query = graphql`
   query homePageQuery {
@@ -52,29 +53,31 @@ const Component = () => {
           <div className="max-w-screen-lg mx-auto">
             <div className="mx-auto flex flex-col justify-center items-center md:mt-0 relative">
               <img src={bg}></img>
-              <div className="z-10 text-center absolute md:px-8 overflow-hidden">
-                <h2 className="text-5xl md:text-8xl font-bold">
-                  Låt oss starta det här tillsammans
+              <div className="z-10 text-center absolute md:px-8 flex flex-col items-center">
+                <h2 className="text-4xl md:text-8xl font-bold mb-4">
+                  {data.title}
                 </h2>
-                <p>Var med och ta del av den nya verkligheten</p>
+                <Typography.DescriptionParagraph>
+                  {data.description}
+                </Typography.DescriptionParagraph>
               </div>
             </div>
 
-            <div className="my-16 grid grid-flow-row md:grid-flow-col py-8 row-auto justify-center gap-6 p-8 md:p-0">
+            <div className="my-16 grid grid-flow-row md:grid-flow-col py-8 row-auto gap-6 p-8 md:p-0">
               {data.getStarted.map((item) => (
                 <div
                   key={item.heading}
-                  className="max-w-xs grid grid-flow-row mb-10 md:mb-0"
+                  className="max-w-xs grid grid-flow-row mb-10 md:mb-0 justify-items-center text-center"
                 >
-                  <div className="w-10 mb-3 md:mb-6 justify-self-center">
+                  <div className="w-10 mb-3 md:mb-6 ">
                     <Img fluid={item.icon.asset.fluid} />
                   </div>
-                  <h3 className="text-2xl font-bold justify-self-center">
-                    {item.heading}
-                  </h3>
-                  <p className="text-center">{item.content}</p>
+                  <h3 className="text-2xl font-bold ">{item.heading}</h3>
+                  <Typography.DescriptionParagraph>
+                    {item.content}
+                  </Typography.DescriptionParagraph>
                   <Link
-                    className="bg-purple-300 rounded p-3 justify-self-center mt-8 self-end"
+                    className="bg-purple-300 hover:bg-purple-200 rounded p-3 mt-8 self-end"
                     to={item.url}
                   >
                     {item.cta}
@@ -83,7 +86,7 @@ const Component = () => {
               ))}
             </div>
 
-            <div className="my-16 md:mb-32 md:mt-64  grid grid-flow-row md:grid-flow-col bg-purple-100 p-8 md:p-16 gap-4">
+            <div className="my-16 md:mb-32 md:mt-64 grid grid-flow-row md:grid-flow-col bg-purple-100 p-8 md:p-16 gap-4">
               {data.focusAreas.map((focusArea) => (
                 <div key={focusArea.heading} className="max-w-xs">
                   <h3 className="text-lg font-bold">{focusArea.heading}</h3>
