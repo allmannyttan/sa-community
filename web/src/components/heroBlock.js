db6aa4bc8606ec417b2ce9e09f003ceb6e7d9ade
momentiris <momentiris@gmail.com>
2021-02-24 14:51:02 +0100

    fix: hero block

--- web/src/components/heroBlock.js
+++ web/src/components/heroBlock.js
@@ -3,41 +3,39 @@ import * as Typography from '../components/typography'
 import HeroImage from './heroImage'
 
 const Component = ({ heroImage, heroText }) => {
-  if (!heroImage.asset && !heroText) return null
-  if (!heroImage.asset && heroText)
+  const image = heroImage || {}
+  if (!image.asset && !heroText) return null
+  if (!image.asset && heroText)
     return (
       <div className="flex items-center justify-center bg-gray-100 w-full h-48">
         <Typography.H1>{heroText}</Typography.H1>
       </div>
     )
 
-  if (!heroImage.asset && heroText) return <HeroImage data={heroImage} />
+  if (!image.asset && heroText) return <HeroImage data={image} />
 
-  if (heroImage.asset && heroText)
-    return (
-      <div className="relative w-full">
-        <div className="relative h-40 sm:h-80 inset-0 ">
-          <HeroImage data={heroImage} />
-        </div>
-
-        <div className="absolute w-full inset-0 flex items-center justify-center text-white ">
-          <Typography.H1>{heroText}</Typography.H1>
-        </div>
-      </div>
-    )
-
-  if (heroImage.asset && !heroText)
+  if (image.asset && !heroText)
     return (
       <div className="relative w-full">
         <div className="relative inset-0">
           <div className="h-80">
-            <HeroImage data={heroImage} />
+            <HeroImage data={image} />
           </div>
         </div>
       </div>
     )
 
-  return null
+  return (
+    <div className="relative w-full">
+      <div className="relative h-40 sm:h-80 inset-0 ">
+        <HeroImage data={image} />
+      </div>
+
+      <div className="absolute w-full inset-0 flex items-center justify-center text-white ">
+        <Typography.H1>{heroText}</Typography.H1>
+      </div>
+    </div>
+  )
 }
 
 export default Component
