510c360b7be0e44156ce40f2b7f79bea571bcf23
izabellalarsson <izzwii.izabella@gmail.com>
2021-03-04 09:23:07 +0100

    fix: add manifest to list of internal links

--- studio/schemas/bodyPortableText.js
+++ studio/schemas/bodyPortableText.js
@@ -62,6 +62,7 @@ export default {
                   { type: 'homePage' },
                   { type: 'aboutUsPage' },
                   { type: 'apiPage' },
+                  { type: 'manifestPage' },
                 ],
               },
             ],
